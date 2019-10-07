a6f5e28ee0114c9c215ac274c441f8851f043207
lightswarm124 <lightswarm@gmail.com>
2019-10-07 16:33:06 -0400

    first commit

---
+++ src/index.js
@@ -0,0 +1,38 @@
+require('dotenv').config();
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const helmet = require('helmet');
+const morgan = require('morgan');
+const mongoose = require("mongoose");
+const usersRoute = require("./routes/user.route");
+
+// defining the Express app
+const app = express();
+
+// adding Helmet to enhance your API's security
+app.use(helmet());
+
+// using bodyParser to parse JSON bodies into JS objects
+app.use(bodyParser.json());
+
+// enabling CORS for all requests (not very secure)
+app.use(cors());
+
+// adding morgan to log HTTP requests
+app.use(morgan('combined'));
+
+//connect to mongodb
+mongoose
+  .connect("mongodb://localhost/nodejsauth", { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log("Connected to MongoDB..."))
+  .catch(err => console.error("Could not connect to MongoDB..."));
+
+// endpoint to return APIs
+app.use("/api/users", usersRoute);
+
+// start the server
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`listening on port ${port}`);
+});
---
+++ src/models/user.model.js
@@ -0,0 +1,51 @@
+require('dotenv').config();
+const jwt = require('jsonwebtoken');
+const Joi = require('@hapi/joi');
+const mongoose = require('mongoose');
+
+//simple schema
+const UserSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+    minlength: 3,
+    maxlength: 50
+  },
+  email: {
+    type: String,
+    required: true,
+    minlength: 5,
+    maxlength: 255,
+    unique: true
+  },
+  password: {
+    type: String,
+    required: true,
+    minlength: 3,
+    maxlength: 255
+  }
+});
+
+const privateKey = process.env.DB_PASS || 'myprivatekey';
+
+//custom method to generate authToken
+UserSchema.methods.generateAuthToken = function() {
+  const token = jwt.sign({ _id: this._id}, privateKey); //get the private key from the config file -> environment variable
+  return token;
+}
+
+const User = mongoose.model('User', UserSchema);
+
+//function to validate user
+function validateUser(user) {
+  const schema = Joi.object({
+    name: Joi.string().min(3).max(50).required(),
+    email: Joi.string().min(5).max(255).required().email(),
+    password: Joi.string().min(3).max(255).required()
+  });
+
+  return schema.validate(user);
+}
+
+exports.User = User;
+exports.validate = validateUser;
